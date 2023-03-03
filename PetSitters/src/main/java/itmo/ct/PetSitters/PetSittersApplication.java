d37933003fd2fc120467ec04d3ed18fea6ef1841
SMUMINOVA <sobira204@gmail.com>
2023-03-03 12:27:28 +0300

    init

---
+++ PetSitters/src/main/java/itmo/ct/PetSitters/PetSittersApplication.java
@@ -0,0 +1,13 @@
+package itmo.ct.PetSitters;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetSittersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetSittersApplication.class, args);
+	}
+
+}
---
+++ PetSitters/src/test/java/itmo/ct/PetSitters/PetSittersApplicationTests.java
@@ -0,0 +1,13 @@
+package itmo.ct.PetSitters;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetSittersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
